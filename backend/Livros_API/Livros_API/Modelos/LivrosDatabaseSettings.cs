6002cfed84307e22082c7a1b5e5607cd1a7276c9
Gustavo Henrique Dias <31208960+GustavoDiias@users.noreply.github.com>
2019-12-08 17:08:11 -0300

    Iniciando o projeto BackEnd (API)

---
+++ backend/Livros_API/Livros_API/Modelos/LivrosDatabaseSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace Livros_API.Modelos
+{
+    public class LivrosDatabaseSettings : ILivrosDatabaseSettings
+    {
+        public string LivrosCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+
+    public interface ILivrosDatabaseSettings
+    {
+        string LivrosCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
